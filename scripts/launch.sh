5dbba8f8a6ef4baa11d4c8aecae143617728b8ec
zack <zkfax@sympatico.ca>
2014-08-27 16:31:10 -0400

    cleaned up ssh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -27,12 +27,12 @@ PI_IP=pi@192.168.0.10
 
 if [ $PI_IP = localhost ] || [ $PI_IP = $MY_IP ] ; then
 
-    sh '~/src/vertex/scripts/vpd.sh $1' &
+    sh '~/src/vertex/scripts/vpd.sh' $1 &
 else
     #   launch Audio process on remote host
-    echo ssh $PI_IP: sh '~/src/vertex/scripts/vpd.sh karplus_strong.pd'
+    echo ssh $PI_IP: sh '~/src/vertex/scripts/vpd.sh' $1
     #
-    ssh $PI_IP sh '~/src/vertex/scripts/vpd.sh karplus_strong.pd' &
+    ssh $PI_IP sh '~/src/vertex/scripts/vpd.sh' $1 &
 fi
 
 
